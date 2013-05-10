ed49d62b64b48acedc0033c1d96d839c8f9879e7
tka.lu <tkatka@gmail.com>
2013-05-11 00:28:52 +0800

    better capture_slim

--- lib/serve/view_helpers.rb
+++ lib/serve/view_helpers.rb
@@ -71,7 +71,16 @@ module Serve #:nodoc:
     end
     alias capture_rhtml capture_erb
     alias capture_erubis capture_erb
-    alias capture_slim capture_erb
+    
+    def capture_slim(&block)
+      old_buffer = eval  "_buf", block.binding
+      eval  "origin_buf,_buf=_buf,[]", block.binding
+      capture_content = yield.to_s
+      eval  "_buf=origin_buf", block.binding
+      capture_content
+    ensure
+      eval  "_buf=origin_buf", block.binding
+    end
 
     def capture(&block)
       capture_method = "capture_#{script_extension}"
